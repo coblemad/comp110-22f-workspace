887a54118e6b368c15eec66d6b9e948f10555c61
coblemad <coblemad@unc.edu>
2022-08-21 18:02:41 -0400

    Finished Excercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,3 @@
+"""My first program for COMP110."""
+_author_= "730544275"
+print("Hello, beautiful world.")
---
+++ lessons/00_stored_program.py
@@ -0,0 +1,5 @@
+from random import random
+random()
+"comp110".isalpha()
+"110". isdigit()
+"comp110" [0].isalpha()
