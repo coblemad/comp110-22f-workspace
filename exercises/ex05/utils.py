8ef47564785a2086f5f285fb6f56d50b0adc5183
coblemad <coblemad@unc.edu>
2022-09-28 22:56:38 -0400

    Finished EX05!

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -22,10 +22,22 @@ def concat(my_first_list: list[int], my_second_list: list[int]) -> int:
         new_list.append(x)
     for x in my_second_list:
         new_list.append(x)
-        print(x)
+    return new_list
 
 
-def sub(a_list: list[int]) -> int:
+def sub(a_list: list[int], first_num: int, second_num: int) -> int:
     """A function that generates a list that is a subset of the given list between the start index and the end minus one."""
-    #while 
-    return 0;
+    new_list: list[int] = []
+    if first_num < 0:
+        starting_place: int = 0
+    else:
+        starting_place = first_num
+    if second_num > len(a_list):
+        ending_place: int = len(a_list)
+    else:
+        ending_place = second_num
+    if len(a_list) == 0 or starting_place > len(a_list) or ending_place <= 0:
+        return []
+    new_list.append(a_list[starting_place])
+    new_list.append(a_list[ending_place - 1])
+    return new_list
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -2,9 +2,7 @@
 __author__ = "730544275"
 
 
-from exercises.ex05.utils import only_evens
-from exercises.ex05.utils import sub
-from exercises.ex05.utils import concat
+from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_only_evens_use1() -> None:
@@ -19,7 +17,7 @@ def test_only_evens_use2() -> None:
     assert only_evens(xs) == []
 
 
-def test_only_evens_edge() -> None:
+def test_only_evens_edge_case() -> None:
     """Edge case to test if correct when no numbers are in the list."""
     xs: list[int] = []
     assert only_evens(xs) == []
@@ -27,20 +25,44 @@ def test_only_evens_edge() -> None:
 
 def test_concat_use1() -> None:
     """Tests if concat function puts two lists together of the same size."""
-    xs:list[int] = [1, 2, 3]
-    xs:list[int] = [1, 2, 3]
-    assert concat(xs) == [1, 2, 3, 1, 2, 3]
+    xs: list[int] = [1, 2, 3]
+    xss: list[int] = [1, 2, 3]
+    assert concat(xs, xss) == [1, 2, 3, 1, 2, 3]
 
 
 def test_concat_use2() -> None:
     """Tests if concat function puts two lists together of different sizes."""
     xs: list[int] = [1, 3, 4, 5, 9]
-    xs: list[int] = [4, 5]
-    assert concat(xs) == [1, 3, 4, 5, 9, 4, 5]
+    xss: list[int] = [4, 5]
+    assert concat(xs, xss) == [1, 3, 4, 5, 9, 4, 5]
 
 
-def test_concat_edge() -> None:
-    """Edge cast to test if concat function leaves an empty list empty."""
+def test_concat_edge_case() -> None:
+    """Edge case to test if concat function leaves an empty list empty."""
     xs: list[int] = []
+    xss: list[int] = []
+    assert concat(xs, xss) == []
+
+
+def test_sub_use1() -> None:
+    """Tests sub function to see if it performs as requested."""
+    xs: list[int] = [10, 20, 30, 40]
+    xss: int = 1
+    xsss: int = 3
+    assert sub(xs, xss, xsss) == [20, 30]
+
+
+def test_sub_use2() -> None:
+    """Tests sub function to see if it performs as requested."""
+    xs: list[int] = [1, 2, 3, 4, 5, 6]
+    xss: int = 2
+    xsss: int = 3
+    assert sub(xs, xss, xsss) == [3, 3]
+
+
+def test_sub_edge_case() -> None:
+    """Tests sub function to see if it returns an empty list when the length of the list is 0."""
     xs: list[int] = []
-    assert concat(xs) == []
+    xss: int = 1
+    xsss: int = 2
+    assert sub(xs, xss, xsss) == []
