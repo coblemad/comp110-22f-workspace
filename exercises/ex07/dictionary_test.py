63de5dfd60c017bc7fe234fd304c2b4cdd00d585
coblemad <coblemad@unc.edu>
2022-10-12 15:26:06 -0400

    trying again

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -7,53 +7,53 @@ from exercises.ex07.dictionary import invert, favorite_color, count
 
 def test_invert_use1() -> None:
     """Tests the first use case for the invert function to see if it changes the of letters as commanded."""
-    my_numbers: dict({'b': 'c', 'r': 'x', 'y': 'z'})
+    my_numbers = ({'b': 'c', 'r': 'x', 'y': 'z'})
     assert invert(my_numbers) == {'c': 'b', 'x':'r', 'z': 'y'}
 
 
 def test_invert_use2() -> None:
     """Tests the second use case for the invert function to see if it flips two words put into the dictionary."""
-    my_numbers: dict({'slay': 'purr'})
+    my_numbers = ({'slay': 'purr'})
     assert invert(my_numbers) == {'purr': 'slay'}
 
 
 def test_invert_edge() -> None:
     """Tests the edge case for the invert function to see if it returns an empty list when given one."""
-    my_numbers: dict({})
+    my_numbers = ({})
     assert invert(my_numbers) == {}
 
 
 def test_favorite_color_use1() -> None:
     """Tests the first use case for the favorite_color function to see if it returns the correct color."""
-    color_list: dict({"Madelyn": "pink", "Molly": "yellow", "Leah": "red", "Alaina": "pink"})
+    color_list = ({"Madelyn": "pink", "Molly": "yellow", "Leah": "red", "Alaina": "pink"})
     assert favorite_color(color_list) == "pink"
 
 
 def test_favorite_color_use2() -> None:
     """Tests the second use case for the favorite_color function to see if it returns the correct color."""
-    color_list: dict({"Trixie": "pink", "Katya": "purple", "Todrick": "purple"})
+    color_list = ({"Trixie": "pink", "Katya": "purple", "Todrick": "purple"})
     assert favorite_color(color_list) == "purple"
 
 
 def test_favorite_color_edge() -> None:
     """Tests the edge case for the favorite_color function."""
-    color_list: dict({})
+    color_list = ({})
     assert favorite_color(color_list) == ""
 
 
 def test_count_use1() -> None: 
     """Tests the first use case for the count function to see if it counts correctly."""
-    my_list: list("Slay", "Slay", "Slay")
+    my_list = ["Slay", "Slay", "Slay"]
     assert count(my_list) == [{"Slay": 3}]
 
 
 def test_count_use2() -> None: 
     """Tests the second use case for the count function to see if it counts one word correctly."""
-    my_list: list("Material", "girl")
+    my_list = ["Material", "girl"]
     assert count(my_list) == [{"Material": 1, "girl": 1}]
 
 
 def test_count_edge() -> None: 
     """Tests the edge case for the count function to see if it returns an empty list."""
-    my_list:  list()
+    my_list = []
     assert count(my_list) == [{}]
